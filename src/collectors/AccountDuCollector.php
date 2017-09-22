fc32a568883dd12028ffb4717ad0b9cd0b364aba
Yurii Myronchuk <bladeroot@gmail.com>
2017-09-22 16:02:13 +0000

    fix ssh options

--- src/collectors/AccountDuCollector.php
+++ src/collectors/AccountDuCollector.php
@@ -18,7 +18,7 @@ class AccountDuCollector extends AbstractCollector
 
     public $aggregation = FileParser::AGGREGATION_LAST;
 
-    public $sshOptions = '-o ConnectTimeout=29 -o BatchMode=yes -o StrictHostKeyChecking=no -o VisualHostKey=no -P222';
+    public $sshOptions = '-o ConnectTimeout=29 -o BatchMode=yes -o StrictHostKeyChecking=no -o VisualHostKey=no -p222';
 
     public function findObjects()
     {
