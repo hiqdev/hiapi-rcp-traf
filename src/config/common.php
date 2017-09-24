24e84587596d25c9d4ccb392bffb62b9673d3553
Yurii Myronchuk <bladeroot@gmail.com>
2017-09-24 12:08:11 +0000

    order collectors in config; add Mail du collector

--- src/config/common.php
+++ src/config/common.php
@@ -14,20 +14,23 @@ return [
             'rcptraf-tool' => [
                 'class' => \hiapi\rcptraf\tools\RcpTrafTool::class,
             ],
-            'rcptraf-tool:server_traf' => [
-                'class' => \hiapi\rcptraf\collectors\ServerTrafCollector::class,
-            ],
-            'rcptraf-tool:server_traf95' => [
-                'class' => \hiapi\rcptraf\collectors\ServerTraf95Collector::class,
-            ],
-            'rcptraf-tool:server_du' => [
-                'class' => \hiapi\rcptraf\collectors\ServerDuCollector::class,
-            ],
             'rcptraf-tool:account_du' => [
                 'class' => \hiapi\rcptraf\collectors\AccountDuCollector::class,
             ],
             'rcptraf-tool:domain_traf' => [
                 'class' => \hiapi\rcptraf\collectors\DomainTrafCollector::class,
+            ],
+            'rcptraf-tool:mail_du' => [
+                'class' => \hiapi\rcptraf\collectors\MailDuCollector::class,
+            ],
+            'rcptraf-tool:server_du' => [
+                'class' => \hiapi\rcptraf\collectors\ServerDuCollector::class,
+            ],
+            'rcptraf-tool:server_traf' => [
+                'class' => \hiapi\rcptraf\collectors\ServerTrafCollector::class,
+            ],
+            'rcptraf-tool:server_traf95' => [
+                'class' => \hiapi\rcptraf\collectors\ServerTraf95Collector::class,
             ],
         ],
     ],
