40cb5f4c421b3804eb5c211624188416fcab2325
Andrii Vasyliev <sol@hiqdev.com>
2017-09-26 15:20:09 +0000

    splitted out `Worker` from AbstractCollector

--- src/collectors/AbstractCollector.php
+++ src/collectors/AbstractCollector.php
@@ -10,6 +10,7 @@
 
 namespace hiapi\rcptraf\collectors;
 
+use hiapi\rcptraf\utils\FileParser;
 use DateTime;
 
 abstract class AbstractCollector
@@ -26,17 +27,9 @@ abstract class AbstractCollector
 
     public $aggregation = FileParser::AGGREGATION_LAST;
 
-    public $logsDir = '/home/LOGS';
+    public $dataDir = '/home/LOGS';
 
-    public $sshPorts = [222, 22];
-
-    public $sshOptions = '-o ConnectTimeout=29 -o BatchMode=yes -o StrictHostKeyChecking=no -o VisualHostKey=no';
-
-    public $configPath = "/usr/local/rcp/etc";
-
-    public $configName = null;
-
-    public $configFormat = "";
+    public $configPath = '';
 
     public function __construct($tool, $type, $params)
     {
@@ -45,14 +38,22 @@ abstract class AbstractCollector
         $this->params = $params;
     }
 
+    public function getType()
+    {
+        return $this->tupe;
+    }
+
     public function collectAll()
     {
         $groups = $this->groupObjects($this->findObjects());
         if (empty($groups)) {
             return true;
         }
+
         foreach ($groups as $group) {
-            $this->collect($group);
+            $parser = new FileParser($this->keys, $this->fields, $this->aggregation);
+            $worker = new Worker($this, $parser, $group['device_ip'], $group['objects']);
+            $worker->collect();
         }
 
         return true;
@@ -73,157 +74,34 @@ abstract class AbstractCollector
 
     abstract public function findObjects();
 
-    protected function findConfigs($group)
+    public function buildConfig($ip)
     {
         return null;
     }
 
-    protected function createConfig($rows = false)
+    public function getFiles()
     {
-        if ($rows === false || $this->configFormat === "") {
-            return false;
-        }
+        $dir = $this->dataDir . '/' . strtoupper($this->type);
+        $curr = new DateTime();
+        $prev = new DateTime('midnight first day of previous month');
 
-        foreach ($rows as $row) {
-            $config[] = vsprintf($this->configFormat, $row);
-        }
-
-        return implode("\n", $config);
+        return [$dir ."/" . $prev->format('Y-m') . "*", $dir . "/" . $curr->format('Y-m') . "*"];
     }
 
-    public function collect(array $group)
+    public function usesSet(array $uses)
     {
-        $port = $this->detectSshPort($group['device_ip']);
-        if (!$port) {
-            return ;
-        }
-
-        $path = $this->saveConfig($group, $port)->copyData($group, $port);
-        if ($path === false) {
-            return;
-        }
-
-        $data = new FileParser($this->keys, $this->fields, $this->aggregation);
-        $data->parse($path);
-        unlink($path);
-        foreach ($group['objects'] as $object => $row) {
-            $this->saveElement($row, $data->getValues($object));
-        }
-    }
-
-    protected function saveElement($row, $values)
-    {
-        if (!$values) {
-            return;
-        }
-        $curr_date = new DateTime();
-        $uses = [];
-        foreach ($values as $date => $fields) {
-            if (!preg_match('/[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}/', $date, $matches)) {
-                continue;
-            }
-
-            $date = $matches[0];
-            try {
-                $z_date = new DateTime($date);
-            } catch (\Exception $e) {
-                continue;
-            }
-
-            /// $z_date->getTimestamp() < $last_date->getTimestamp()
-            if ($z_date->getTimestamp() > $curr_date->getTimestamp()) {
-                continue;
-            }
-            foreach ($fields as $field => $value) {
-                $uses[] = [
-                    'object_id' => $row['object_id'],
-                    'type'      => $field,
-                    'time'      => $date,
-                    'amount'    => $value,
-                ];
-            }
-        }
         if ($uses) {
             $this->tool->base->usesSet($uses);
         }
     }
 
-    protected function getTmpFileName($ip, $add = '')
+    protected function renderConfig($rows, $format)
     {
-        $dest = '/tmp/' . $this->type . '.' . getmypid() . '.' . $ip . ($add ? ".{$add}" : '');
-        if (false === file_put_contents($dest, '')) {
-            throw new \Exception("failed copy traf data to $dest");
+        $res = '';
+        foreach ($rows as $row) {
+            $res .= vsprintf($format, $row) . "\n";
         }
 
-        return $dest;
-    }
-
-    protected function copyFile($src, $dest, $port)
-    {
-        // FOR PRODUCTION USE UNCOMENT NEXT LINE
-//        exec("/usr/bin/scp {$this->sshOptions} -P{$port} $src $dest", $output, $res);
-        return $res == 0;
-    }
-
-    protected function saveConfig($group, $port = 222)
-    {
-        if ($this->configPath === null || $this->configName === null) {
-            return $this;
-        }
-
-        $config = $this->createConfig($this->findConfigs($group));
-        if ($config === false) {
-            return $this;
-        }
-
-        $src = $this->getTmpFileName($group['device_ip'], $this->configName);
-        if (false === file_put_contents($src, $config)) {
-            throw new \Exception("failed copy traf config to $src");
-        }
-
-        $dst = "root@{$group['device_ip']}:{$this->configPath}/{$this->configName}";
-        if ($this->copyFile($src, $dst, $port)=== false){
-            throw new \Exception("failed copy traf config to $dst");
-        }
-
-        unlink($src);
-        return $this;
-    }
-
-    protected function copyData($group, $port = 222)
-    {
-        $ip = $group['device_ip'];
-        $dest = $this->getTmpFileName($ip);
-        $files = implode(" ", $this->getFiles());
-        $command = "ssh {$this->sshOptions} -p{$port} root@$ip '/bin/cat $files' > $dest 2>/dev/null";
-        exec($command, $output, $result);
-
-        return $dest;
-    }
-
-    protected function detectSshPort($ip)
-    {
-        foreach ($this->sshPorts as $port) {
-            if ($socket = @fsockopen($ip, $port, $errno, $errstr, 3)) {
-                fclose($socket);
-
-                return $port;
-            }
-        }
-
-        return false;
-    }
-
-    protected function getFiles()
-    {
-        static $dir;
-        if ($dir === null) {
-            $dir = $this->logsDir . '/' . strtoupper($this->type);
-        }
-
-        $curr = new DateTime();
-        $prev = new DateTime('midnight first day of previous month');
-
-        return [$dir ."/" . $prev->format('Y-m') . "*", $dir . "/" . $curr->format('Y-m') . "*"];
+        return $res;
     }
 }
---
+++ src/collectors/Worker.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * hiAPI RCP Traf Collector
+ *
+ * @link      https://github.com/hiqdev/hiapi-rcp-traf
+ * @package   hiapi-rcp-traf
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiapi\rcptraf\collectors;
+
+use DateTime;
+use hiapi\rcptraf\utils\FileParser;
+use hiapi\rcptraf\utils\Ssh;
+
+class Worker
+{
+    public $tmpDir = '/tmp';
+
+    protected $collector;
+    protected $parser;
+    protected $ip;
+    protected $objects;
+
+    public function __construct(AbstractCollector $collector, FileParser $parser, $ip, $objects)
+    {
+        $this->collector = $collector;
+        $this->parser = $parser;
+        $this->ip = $ip;
+        $this->objects = $objects;
+    }
+
+    public function collect()
+    {
+        $this->ssh = new Ssh($this->ip);
+        if (!$this->ssh->canConnect()) {
+            return false;
+        }
+
+        $this->putConfig();
+
+        return $this->saveData();
+    }
+
+    protected function saveData()
+    {
+        $path = $this->copyData();
+        if (!$path) {
+            return false;
+        }
+        $this->parser->parse($path);
+        unlink($path);
+
+        foreach ($this->objects as $object => $row) {
+            $this->saveElement($row, $this->parser->getValues($object));
+        }
+
+        return true;
+    }
+
+    protected function saveElement($row, $values)
+    {
+        if (!$values) {
+            return;
+        }
+        $curr_date = new DateTime();
+        $uses = [];
+        foreach ($values as $date => $fields) {
+            if (!preg_match('/[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}/', $date, $matches)) {
+                continue;
+            }
+
+            $date = $matches[0];
+            try {
+                $z_date = new DateTime($date);
+            } catch (\Exception $e) {
+                continue;
+            }
+
+            /// $z_date->getTimestamp() < $last_date->getTimestamp()
+            if ($z_date->getTimestamp() > $curr_date->getTimestamp()) {
+                continue;
+            }
+            foreach ($fields as $field => $value) {
+                $uses[] = [
+                    'object_id' => $row['object_id'],
+                    'type'      => $field,
+                    'time'      => $date,
+                    'amount'    => $value,
+                ];
+            }
+        }
+        $this->collector->usesSet($uses);
+    }
+
+    protected function putConfig()
+    {
+        $config = $this->collector->buildConfig($this->ip);
+        if ($config === null) {
+            return null;
+        }
+
+        $src = $this->getTmpFile('config', $config);
+        $this->ssh->put($src, $this->collector->configPath);
+        unlink($src);
+
+        return true;
+    }
+
+    protected function getTmpFile($postfix = '', $content = '')
+    {
+        $parts = array_filter([$this->collector->getType(), getmypid(), $this->ip, $postfix]);
+        $dest = $this->tmpDir . '/' . implode('.', $parts);
+        if (false === file_put_contents($dest, $content)) {
+            throw new \Exception("failed write tmp file $dest");
+        }
+
+        return $dest;
+    }
+
+    protected function copyData()
+    {
+        $dest = $this->getTmpFile('data');
+        $files = implode(" ", $this->collector->getFiles());
+        $this->ssh->run("/bin/cat $files", "> $dest 2> /dev/null");
+
+        return $dest;
+    }
+
+}
