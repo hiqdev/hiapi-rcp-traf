295c92d1b967dcc6722e41ec0e90db53e9951472
Andrii Vasyliev <sol@hiqdev.com>
2019-02-15 17:02:15 +0200

    Making Yii 2/3 compatible config

--- src/config/common.php
+++ src/config/common.php
@@ -8,35 +8,33 @@
  * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
  */
 
-return [
-    'container' => [
-        'definitions' => [
-            'rcptrafTool' => [
-                '__class' => \hiapi\rcptraf\RcpTrafTool::class,
-            ],
-            'rcptrafTool:account_du' => [
-                '__class' => \hiapi\rcptraf\collectors\AccountDuCollector::class,
-            ],
-            'rcptrafTool:domain_traf' => [
-                '__class' => \hiapi\rcptraf\collectors\DomainTrafCollector::class,
-            ],
-            'rcptrafTool:ip_traf' => [
-                '__class' => \hiapi\rcptraf\collectors\IpTrafCollector::class,
-            ],
-            'rcptrafTool:mail_du' => [
-                '__class' => \hiapi\rcptraf\collectors\MailDuCollector::class,
-            ],
-            'rcptrafTool:server_du' => [
-                '__class' => \hiapi\rcptraf\collectors\ServerDuCollector::class,
-            ],
-            'rcptrafTool:server_traf' => [
-                '__class' => \hiapi\rcptraf\collectors\ServerTrafCollector::class,
-                'configPath' => $params['rcptrafTool:server_traf.configPath'],
-            ],
-            'rcptrafTool:server_traf95' => [
-                '__class' => \hiapi\rcptraf\collectors\ServerTraf95Collector::class,
-                'configPath' => $params['rcptrafTool:server_traf95.configPath'],
-            ],
-        ],
+$definitions = [
+    'rcptrafTool' => [
+        '__class' => \hiapi\rcptraf\RcpTrafTool::class,
+    ],
+    'rcptrafTool:account_du' => [
+        '__class' => \hiapi\rcptraf\collectors\AccountDuCollector::class,
+    ],
+    'rcptrafTool:domain_traf' => [
+        '__class' => \hiapi\rcptraf\collectors\DomainTrafCollector::class,
+    ],
+    'rcptrafTool:ip_traf' => [
+        '__class' => \hiapi\rcptraf\collectors\IpTrafCollector::class,
+    ],
+    'rcptrafTool:mail_du' => [
+        '__class' => \hiapi\rcptraf\collectors\MailDuCollector::class,
+    ],
+    'rcptrafTool:server_du' => [
+        '__class' => \hiapi\rcptraf\collectors\ServerDuCollector::class,
+    ],
+    'rcptrafTool:server_traf' => [
+        '__class' => \hiapi\rcptraf\collectors\ServerTrafCollector::class,
+        'configPath' => $params['rcptrafTool:server_traf.configPath'],
+    ],
+    'rcptrafTool:server_traf95' => [
+        '__class' => \hiapi\rcptraf\collectors\ServerTraf95Collector::class,
+        'configPath' => $params['rcptrafTool:server_traf95.configPath'],
     ],
 ];
+
+return class_exists('Yii') ? ['container' => ['definitions' => $definitions]] : $definitions;
